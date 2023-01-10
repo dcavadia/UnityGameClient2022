a062a7d6ff7886499321145f9dccf53aa8a4167b
dcavadia <dan.acppp@gmail.com>
2023-01-10 01:29:50 -0400

    rework player network

--- Assets/Scripts/Player/PlayerNetwork.cs
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -5,33 +5,78 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
-    private readonly NetworkVariable<PlayerNetworkData> _netState = new (writePerm: NetworkVariableWritePermission.Owner);
-    private Vector3 _vel;
-    private float _rotVel;
+    [SerializeField] private bool _serverAuth;
     [SerializeField] private float _cheapInterpolationTime = 0.1f;
 
+    private NetworkVariable<PlayerNetworkState> _playerState;
+    private Rigidbody _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+        var permission = _serverAuth ? NetworkVariableWritePermission.Server : NetworkVariableWritePermission.Owner;
+        _playerState = new NetworkVariable<PlayerNetworkState>(writePerm: permission);
+    }
 
     void Update()
     {
         if (IsOwner)
         {
-            _netState.Value= new PlayerNetworkData()
-            {
-                Position = transform.position,
-                Rotation = transform.rotation.eulerAngles
-            };
+            TransmitState();
         }
         else
         {
-            transform.position = Vector3.SmoothDamp(transform.position, _netState.Value.Position, ref _vel, _cheapInterpolationTime);
-            transform.rotation = Quaternion.Euler(
-                0,
-                Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, _netState.Value.Rotation.y, ref _rotVel, _cheapInterpolationTime),
-                0);
+            ConsumeState();
         }
     }
 
-    struct PlayerNetworkData : INetworkSerializable
+    public override void OnNetworkSpawn()
+    {
+        if(!IsOwner)
+        {
+            //Destroy(transform.GetComponent<PlayerController>());
+        }
+    }
+
+    private void TransmitState()
+    {
+        var state = new PlayerNetworkState
+        {
+            Position = _rb.position,
+            Rotation = transform.rotation.eulerAngles
+        };
+
+        if(IsServer || !_serverAuth)
+        {
+            _playerState.Value= state;
+        }
+        else
+        {
+            TransmitStateServerRpc(state);
+        }
+    }
+
+    [ServerRpc]
+    private void TransmitStateServerRpc(PlayerNetworkState state)
+    {
+        _playerState.Value = state;
+    }
+
+    private Vector3 _posVel;
+    private float _rotVelY;
+
+    private void ConsumeState()
+    {
+        _rb.MovePosition(Vector3.SmoothDamp(_rb.position, _playerState.Value.Position, ref _posVel, _cheapInterpolationTime));
+        
+        transform.rotation = Quaternion.Euler(
+            0,
+            Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, _playerState.Value.Rotation.y, ref _rotVelY, _cheapInterpolationTime),
+            0);
+    }
+
+    struct PlayerNetworkState : INetworkSerializable
     {
         private float _x, _z;
         private short _yRot;
